d88e345a63a80b41de7f080b93e9e20fe55b7bbc
android <30924011+psing4@users.noreply.github.com>
2019-07-08 19:40:47 +0900

    ColorToGray
    how to use convert Color

---
+++ ColorToGrayByArray/ColorToGrayByArray/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ColorToGrayByArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOpen = new System.Windows.Forms.Button();
+            this.btGrayBright = new System.Windows.Forms.Button();
+            this.ofdOpen = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(162, 27);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(75, 23);
+            this.btOpen.TabIndex = 0;
+            this.btOpen.Text = "열기";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.BtOpen_Click);
+            // 
+            // btGrayBright
+            // 
+            this.btGrayBright.Location = new System.Drawing.Point(625, 27);
+            this.btGrayBright.Name = "btGrayBright";
+            this.btGrayBright.Size = new System.Drawing.Size(75, 23);
+            this.btGrayBright.TabIndex = 1;
+            this.btGrayBright.Text = "그레이 ";
+            this.btGrayBright.UseVisualStyleBackColor = true;
+            this.btGrayBright.Click += new System.EventHandler(this.BtGrayBright_Click);
+            // 
+            // ofdOpen
+            // 
+            this.ofdOpen.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btGrayBright);
+            this.Controls.Add(this.btOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Button btGrayBright;
+        private System.Windows.Forms.OpenFileDialog ofdOpen;
+    }
+}
+
---
+++ ColorToGrayByArray/ColorToGrayByArray/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorToGrayByArray
+{
+    public partial class Form1 : Form
+    {
+        Image image;
+        Bitmap gBitmap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtOpen_Click(object sender, EventArgs e)
+        {
+            string filiterName = "All Files(*.*)|*.*|Bitmpap File(*.bmp)|*.bmp|"; //파일 확장자 추가 
+            filiterName = filiterName + "Gif File(*.gif)|*.gif|Jpeg File(*.jpg)|*.jpg"; //파일 확장자 추가
+            Graphics gr = CreateGraphics();//Form아래의 메소드 
+            ofdOpen.Title = "파일열기"; //파일열기 다이얼로그의 이름 
+
+            ofdOpen.Filter = filiterName;
+            if (ofdOpen.ShowDialog() == DialogResult.OK)
+            {
+                string strFilname = ofdOpen.FileName;
+                image = Image.FromFile(strFilname);//static 
+                gr.DrawImage(image, 10, 50, image.Width, image.Height);
+            }
+            gr.Dispose();
+
+        }
+
+        private void BtGrayBright_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            int brightness;
+            const int ADD_VALUE = 50;
+            Color color;
+            int[,] grayArray = new int[image.Height, image.Width];
+            Graphics gr = CreateGraphics();
+            gBitmap = new Bitmap(image);
+            for(y=0;y<image.Height;y++)
+            {
+                for(x=0;x<image.Width;x++)
+                {
+                    color = gBitmap.GetPixel(x, y);
+                    brightness = (int)(0.299 * color.R + 0.587 * color.G + 0.114 * color.B);
+
+                    grayArray[y, x] = brightness;
+
+                }
+            }
+            for (y = 0; y < image.Height; y++)
+            {
+                for (x = 0; x < image.Width; x++)
+                {
+                    grayArray[y, x] += ADD_VALUE;
+                    if (grayArray[y, x] > 255) grayArray[y, x] = 255;
+
+                }
+            }
+            for (y = 0; y < image.Height; y++)
+            {
+                for (x = 0; x < image.Width; x++)
+                {
+                    color = Color.FromArgb(grayArray[y, x], grayArray[y, x], grayArray[y, x]);
+                    gBitmap.SetPixel(x, y, color);
+                }
+            }
+            gr.DrawImage(gBitmap, 20 + gBitmap.Width, 50, gBitmap.Width, gBitmap.Height);
+            gr.Dispose();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = CreateGraphics();
+            if (image == null)
+            {
+
+            }
+            else
+            {
+                if(gBitmap == null)
+                {
+
+                }
+                else
+                {
+                    graphics.DrawString("졸려", Font, Brushes.Black, 10, 10);
+                    graphics.DrawImage(image, 10, 50, image.Width, image.Height);
+                    graphics.DrawImage(gBitmap, 20 + gBitmap.Width, 50, gBitmap.Width, gBitmap.Height);
+                }
+                
+            }
+            graphics.Dispose();//
+
+        }
+    }
+}
